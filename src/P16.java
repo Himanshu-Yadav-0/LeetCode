4c6aa38a5677b3c28b6e5b1c1b461531035dde47
Himanshu Yadav <himanshuydv0004@gmail.com>
2025-01-19 13:32:23 +0530

    Three Sum Closest

--- src/P16.java
+++ src/P16.java
@@ -1,6 +1,32 @@
-public class P16 {
+import java.util.Arrays;
 
+public class P16 {
+    public static int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int resultSum = nums[0]+nums[1]+nums[2];
+        int minDifference = Integer.MAX_VALUE;
+        for(int i=0 ;i<nums.length;i++){
+            int left  = i+1;
+            int right = nums.length-1;
+            while(left<right){
+                int sum = nums[i]+nums[left]+nums[right];
+                if(sum==target)return sum;
+                else if(sum<target)left++;
+                else right--;
+
+                int difToTarget= Math.abs(sum-target);
+                if(minDifference>difToTarget){
+                    resultSum = sum;
+                    minDifference=difToTarget;
+                }
+            }
+        }
+        return resultSum;
+
+    }
     public static void main(String[] args) {
-        System.out.println(1+2);
+        int[] nums = {0,0,0,0};
+        int target = 1;
+        System.out.println(threeSumClosest(nums,target));
     }
 }
